b7e365a8ceb9dcdeee4f591fd1ace5f0850aacf0
AtharvaThorve <aaathorve@gmail.com>
2021-08-24 11:05:01 +0530

    Removed old login page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/",(req, res) => {
-  res.render("newLogin");
+  res.render("login");
 });
 
 app.post("/",formController.formController);
