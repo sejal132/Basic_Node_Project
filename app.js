baba63920241e78ece3fcbbc7c6599e280af571b
sejal132 <sejalagrawal01@gmail.com>
2021-08-24 13:00:55 +0530

    dashboard added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/",(req, res) => {
-  res.render("login");
+  res.render("dashboard");
 });
 
 app.post("/",formController.formController);
